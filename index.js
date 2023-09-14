a006a2cb1bfe4dd3e67e82031fa51b0a3afc8e4c
TheIanAnderson <131201726+TheIanAnderson@users.noreply.github.com>
2023-09-14 14:58:39 -0500

    added additional functions to interact with database, create, put, delete ect.

--- index.js
+++ index.js
@@ -100,3 +100,133 @@ function viewEmployees() {
   });
 }
 
+function updateEmployeeRole() {
+  connection.query("SELECT id, name FROM employees", (err, employeeRows) => {
+    if (err) throw err;
+
+    connection.query("SELECT id, name FROM roles", (err, roleRows) => {
+      if (err) throw err;
+
+      inquirer
+        .prompt([
+          {
+            name: "employeeId",
+            type: "list",
+            message: "Select the employee to update:",
+            choices: employeeRows.map((employee) => ({
+              name: employee.name,
+              value: employee.id,
+            })),
+          },
+          {
+            name: "roleId",
+            type: "list",
+            message: "Select the new role:",
+            choices: roleRows.map((role) => ({
+              name: role.name,
+              value: role.id,
+            })),
+          },
+        ])
+        .then((answers) => {
+          // Update the employee's role in the database
+          connection.query(
+            "UPDATE employees SET role_id = ? WHERE id = ?",
+            [answers.roleId, answers.employeeId],
+            (err) => {
+              if (err) throw err;
+              console.log("Employee role updated successfully.");
+              startApp();
+            }
+          );
+        });
+    });
+  });
+}
+
+function deleteAnEmployee() {
+  connection.query("SELECT id, name FROM employees", (err, employeeRows) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: "employeeId",
+          type: "list",
+          message: "Select the employee to delete:",
+          choices: employeeRows.map((employee) => ({
+            name: employee.name,
+            value: employee.id,
+          })),
+        },
+      ])
+      .then((answer) => {
+        const employeeIdToDelete = answer.employeeId;
+
+        connection.query(
+          "DELETE FROM employees WHERE id = ?",
+          [employeeIdToDelete],
+          (err) => {
+            if (err) throw err;
+            console.log("Employee deleted successfully.");
+            startApp();
+          }
+        );
+      });
+  });
+}
+
+async function createNewEmployee() {
+  try {
+    const roles = await fetchRolesFromDatabase();
+
+    inquirer
+      .prompt([
+        {
+          name: "name",
+          type: "input",
+          message: "Enter the employee name:",
+        },
+        {
+          name: "roleId",
+          type: "list",
+          message: "Select the employee role:",
+          choices: roles,
+        },
+      ])
+      .then((answers) => {
+        connection.query(
+          "INSERT INTO employees (name, role_id) VALUES (?, ?)",
+          [answers.name, answers.roleId],
+          (err) => {
+            if (err) throw err;
+            console.log("New employee added successfully.");
+            startApp();
+          }
+        );
+      });
+  } catch (error) {
+    console.error("Error fetching roles:", error);
+    startApp();
+  }
+}
+
+function fetchRolesFromDatabase() {
+  return new Promise((resolve, reject) => {
+    connection.query("SELECT id, name FROM roles", (err, rows) => {
+      if (err) reject(err);
+      resolve(
+        rows.map((role) => ({
+          name: role.name,
+          value: role.id,
+        }))
+      );
+    });
+  });
+}
+
+//NOTES:
+// DEPARTMENTS, ROLES, and EMPLOYEES
+// EMPLOYEES: Row, First & Last Name, Birthday, Availablilty?
+// ROLES: Job Title, Employee Assigned to it, and Description.
+// DEPARTMENTS: Different department titles
